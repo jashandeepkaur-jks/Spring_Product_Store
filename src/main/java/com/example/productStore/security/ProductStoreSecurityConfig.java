4fbeca587091ffc52a0e05bd3fe1314365a38b10
Jashandeep Kaur <jkaur@xpanxion.com>
2020-05-11 20:34:14 +0530

    updated jwt config

--- src/main/java/com/example/productStore/security/ProductStoreSecurityConfig.java
+++ src/main/java/com/example/productStore/security/ProductStoreSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.productStore.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -50,9 +51,12 @@ public class ProductStoreSecurityConfig extends WebSecurityConfigurerAdapter {
 // We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 // dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().
+				.authorizeRequests().antMatchers("/authenticate").
+				permitAll().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().
+
+				
 // all other requests need to be authenticated
-				//anyRequest().authenticated().
+				anyRequest().authenticated().
 				and().
 // make sure we use stateless session; session won't be used to
 // store user's state.
